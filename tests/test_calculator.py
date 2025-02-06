d8ad6cd1b4fbf665581fa055561e08169f8bae84
Kallam Jai Bharath Reddy <bharathreddy@Kallams-MacBook-Air.local>
2025-02-06 16:57:37 -0500

    performed subtraction in calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,6 @@
-from calculator import add
-def test_add():
+from calculator import add, subtract
+def test_addition():
     assert add(3, 2) == 5
+
+def test_subtraction():
+    assert subtract(7, 4) == 3
